8cefd46537b6bc43d747f05032b0a152cfe2b867
ovkachanov <ovkachanov1988@gmail.com>
2018-05-18 15:34:50 +0300

    add spec Game .current_game_question & previous_level

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -83,4 +83,15 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:money)
     end
   end
+
+  context 'course of the game' do
+    it '.previous_level' do
+      game_w_questions.current_level = 7
+      expect(game_w_questions.previous_level).to eq(6)
+    end
+
+    it '.current_game_question' do
+      expect(game_w_questions.current_game_question).to eq(game_w_questions.game_questions[0])
+    end
+  end
 end
